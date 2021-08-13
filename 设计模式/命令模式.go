12771c2e75e25a25edf9376f25bb7380bc514d13
yangxk <yangxk@163.com>
2021-08-13 17:19:45 +0800

    complete some questions ok

--- 设计模式/命令模式.go
+++ 设计模式/命令模式.go
@@ -1,1 +1,63 @@
 package main
+
+import "fmt"
+
+type command interface {
+	execute()
+}
+
+type device interface {
+	on()
+	off()
+}
+
+type button struct {
+	command command
+}
+
+func (b *button) press() {
+	b.command.execute()
+}
+
+type onCommand struct {
+	device device
+}
+
+func (c *onCommand) execute() {
+	c.device.on()
+}
+
+type offCommand struct {
+	device device
+}
+
+func (c *offCommand) execute() {
+	c.device.off()
+}
+
+type tv struct {
+	isRunning bool
+}
+
+func (t *tv) on() {
+	t.isRunning = true
+	fmt.Println("Turning tv on")
+}
+
+func (t *tv) off() {
+	t.isRunning = false
+	fmt.Println("Turning tv off")
+}
+
+func main() {
+	tv := &tv{}
+
+	onCommand := &onCommand{device: tv}
+	offCommand := &offCommand{device: tv}
+
+	onButton := &button{command: onCommand}
+	onButton.press()
+
+	offButton := &button{command: offCommand}
+	offButton.press()
+}
